8c75b98b779b4846c5f98212f78961e27e03b1cf
Erika <127626053+ErikaLouise@users.noreply.github.com>
2023-08-14 21:07:04 -0300

    q1.c

---
+++ q1.c
@@ -0,0 +1,23 @@
+# include <stdio.h>
+
+int main(){
+
+    char c;
+    int i = 0;
+    int count = 0;
+
+do{
+c = i;
+printf(" %c", c);
+count ++;
+ if (count == 10){
+    printf ("\n");
+    count = 0;
+ }
+
+i++;
+
+}while (i<= 127);
+
+return 0;
+}
